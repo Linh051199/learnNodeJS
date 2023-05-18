e0eccbaf0c73a20fcaddbbc240394366afd23333
Linh051199 <phamvanlinh0511@gmail.com>
2023-05-18 10:56:49 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const express = require("express");
+const { engine } = require("express-handlebars");
+const morgan = require("morgan");
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//Http logger
+app.use(morgan("combined"));
+
+//Template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
